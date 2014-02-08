3bf7173ae22c99f22cba9f8b5a0f3f9ef0d50bfb
livtoler <livtoler@gmail.com>
2014-02-08 16:19:30 -0600

    Murray Me

---
+++ javascripts/card-flip.js
@@ -0,0 +1,21 @@
+/*
+ * $(document).ready() is a helper function that waits until the jQuery library is fully loaded in your Browser before it tries to use it.
+*/
+$(document).ready(function() {
+  /*
+   * This function waits and listens for a "click" to happen on the div with the class name "hackentines-day-card-container"
+  */
+  $(".hackentines-day-card-container").on("click", function() {
+    $(".card").toggle();
+    $("#santori").fadeOut(4300);
+    $("p.red-text").hide().fadeIn(15000);
+    
+    /*
+      If you like, Try this instead!
+      $(".card").slideToggle();
+      or, try this!
+      $(".card").fadeToggle();
+    */
+  });
+
+});
